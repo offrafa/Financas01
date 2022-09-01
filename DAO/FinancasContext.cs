ed97bbbf2d8cd36f2f3192ddf23b0eb56c82f3ce
Rafael <rafaeltavares462@gmail.com>
2022-09-01 04:27:55 -0300

    Mapeando as entidades usuario e movimentação

---
+++ DAO/FinancasContext.cs
@@ -0,0 +1,17 @@
+﻿using Financas01.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas01.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Movimentacao> movimentacoes { get; set; }
+
+        
+    }
+}
