6b4db517ec2e816392737094b7415591fb2722e2
Rafael <rafaeltavares462@gmail.com>
2022-09-04 17:09:59 -0300

    Adicionando UsuarioController

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas01.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
