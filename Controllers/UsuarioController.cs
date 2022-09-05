40bbf88df1fe7b59fc43181879755516d2f79c0d
Rafael <rafaeltavares462@gmail.com>
2022-09-04 21:05:58 -0300

    Criando o metodo para adiciona usuario no banco de dados

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Financas01.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,24 @@ using System.Web.Mvc;
 
 namespace Financas01.Controllers
 {
-    public class UsuarioController : Controller
+    public class UsuarioController : Controller 
     {
         // GET: Usuario
         public ActionResult Form()
         {
             return View();
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
         }
     }
 }
---
+++ DAO/UsuarioDAO.cs
@@ -0,0 +1,27 @@
+﻿using Financas01.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas01.DAO
+{
+    public class UsuarioDAO
+    {
+        private FinancasContext Context;
+        public UsuarioDAO(FinancasContext context)
+        {
+            this.Context = context;
+        }
+
+        public void Adiciona(Usuario usuario)
+        {
+
+        }
+
+        public IList<Usuario> Lista()
+        {
+
+        }
+    }
+}
