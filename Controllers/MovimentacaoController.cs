b51e86653b66a0c469dc9e3d14475b747d6140ec
Rafael <rafaeltavares462@gmail.com>
2022-09-06 04:20:40 -0300

    Adicionando controller para movimentações

---
+++ Controllers/MovimentacaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas01.Controllers
+{
+    public class MovimentacaoController : Controller
+    {
+        // GET: Movimentacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
